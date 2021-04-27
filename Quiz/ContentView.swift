97cd9413f22f7b4df2b1af0f4568ec7ecff9ae1c
Andy Lochan <36180382+Andylochan@users.noreply.github.com>
2021-04-26 22:40:19 -0400

    Initial Commit

---
+++ Quiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quiz
+//
+//  Created by Andy Lochan on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Andy Lochan on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
